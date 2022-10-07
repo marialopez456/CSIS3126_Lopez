57150f3c5896c902432d453e0f6edcc7ab6bac67
marialopez456 <umalopez123@gmail.com>
2022-10-07 15:23:58 -0400

    Second commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+app.set('view-engine','ejs')
+app.get('/',(req,res)=>{
+    res.render('index.ejs')
+})
+app.get('/',(req,res)=>{
+    res.render('login.ejs')
+})
+app.get('/',(req,res)=>{
+    res.render('register.ejs')
+})
+app.listen(3000)
